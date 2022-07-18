b76da02d5d59808a38d19cff339012b13335321a
Philipp Borgers <borgers@mi.fu-berlin.de>
2022-07-18 16:16:07 +0200

    Install mavproxy fork for now
    Installs my mavproxy fork that receives position information from all
    constellations not just one.

--- stage3/searchwing-antennabox/01-run.sh
+++ stage3/searchwing-antennabox/01-run.sh
@@ -30,7 +30,7 @@ on_chroot << EOF
 EOF
 
 on_chroot << EOF
-  pip install mavproxy
+  pip install git+https://github.com/booo/mavproxy@accept-position-from-other-constellations
   pip install pynmea2
 EOF
 
