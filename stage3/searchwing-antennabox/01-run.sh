28f885d035fdd7cb2a98f6e981edcbe5a0fcdde0
Philipp Borgers <borgers@mi.fu-berlin.de>
2023-03-25 12:41:45 +0100

    hostapd: Disable hostapd
    We integrated a WiFi accesspoint/router into the system and don't need
    hostapd and an accesspoint on the PI. I leave the hostapd configuration
    in place for future use.

--- stage3/searchwing-antennabox/01-run.sh
+++ stage3/searchwing-antennabox/01-run.sh
@@ -34,8 +34,6 @@ on_chroot << EOF
   sudo systemctl enable mavlink-router.service
   sudo systemctl enable searchwing-mavproxy.service
   sudo systemctl enable systemd-networkd
-  sudo systemctl unmask hostapd
-  sudo systemctl enable hostapd
   sudo systemctl enable dnsmasq
 
   ln -sf /usr/share/zoneinfo/UTC /etc/localtime
